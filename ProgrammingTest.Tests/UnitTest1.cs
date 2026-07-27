472dcbc2a2c81cb00d5caed3960703c8c9ef4c31
Steve Francis King Pada <padastevefrancisking@gmail.com>
2026-07-28 02:51:16 +0800

    Add project files.

---
+++ ProgrammingTest.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ProgrammingTest.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
